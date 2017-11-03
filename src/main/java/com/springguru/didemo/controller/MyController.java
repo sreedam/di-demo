40e10570ebdce84706d1a3513b9d8c92d0521e7c
sreedam <sreedamhembram21@gmail.com>
2017-11-03 10:11:06 +0530

    Initial commit

---
+++ src/main/java/com/springguru/didemo/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.springguru.didemo.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String hello()
+    {
+        return "";
+    }
+}
